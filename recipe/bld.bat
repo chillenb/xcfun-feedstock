d37f3073d92b262fcd98a9d189227848aa05500e
Christopher Hillenbrand <chillenbrand15@gmail.com>
2026-09-09 19:56:20 -0400

    workaround for stupid windows cmake nonsense

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,15 +4,15 @@ cmake -H"%SRC_DIR%" ^
       -GNinja ^
       -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
       -DCMAKE_BUILD_TYPE=Release ^
-      -DCMAKE_INSTALL_LIBDIR="Library\lib" ^
-      -DPYMOD_INSTALL_LIBDIR="..\..\Lib\site-packages" ^
+      -DCMAKE_INSTALL_LIBDIR="Library/lib" ^
+      -DPYMOD_INSTALL_LIBDIR="../../Lib/site-packages" ^
       -DXCFUN_MAX_ORDER=8 ^
       -DXCFUN_PYTHON_INTERFACE=ON ^
       -DCMAKE_CXX_FLAGS="/wd4018 /wd4101 /wd4996 /EHsc" ^
-      -DCMAKE_INSTALL_INCLUDEDIR="Library\include" ^
-      -DCMAKE_INSTALL_BINDIR="Library\bin" ^
+      -DCMAKE_INSTALL_INCLUDEDIR="Library/include" ^
+      -DCMAKE_INSTALL_BINDIR="Library/bin" ^
       -DCMAKE_INSTALL_DATADIR="Library" ^
-      -DDEF_INSTALL_CMAKEDIR="Library\share\cmake\XCFun" ^
+      -DDEF_INSTALL_CMAKEDIR="Library/share/cmake/XCFun" ^
       -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=true
 if errorlevel 1 exit 1
 
